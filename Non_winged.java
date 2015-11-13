c0ef812952bec075914fb0ccfa8cb55243485657
Similoluwa Falaye <simifalaye1@gmail.com>
2015-11-12 22:49:34 -0600

    rev 4

---
+++ Non_winged.java
@@ -0,0 +1,20 @@
+/**
+ * Info: 
+ * 2015-10-20
+ * @author Similoluwa Falaye
+ */
+import java.util.*;
+  
+ public class Non_winged extends Animal{
+	private int  maxTravelDistance;
+	
+	public Non_winged()
+	{
+		this.maxTravelDistance = 5;
+	}
+	
+	public int getMaxTravelDistance()
+	{
+		return this.maxTravelDistance;
+	}
+ }
